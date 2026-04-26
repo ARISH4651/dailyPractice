cab9d2a471d0c5f9ac59e7750f086d5d2d2e9c1f
Arish K <147861235+ARISH4651@users.noreply.github.com>
2026-04-26 19:32:27 +0530

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length()>s2.length()) return false;
+        int[]count1=new int[26];
+        int[]count2=new int[26];
+        for(int i=0;i<s1.length();i++){
+            count1[s1.charAt(i)-'a']++;
+            count2[s2.charAt(i)-'a']++;
+        }
+        if (Arrays.equals(count1,count2)) return true;
+        int l=0;
+        for(int r=s1.length();r<s2.length();r++){
+            count2[s2.charAt(r)-'a']++;
+            count2[s2.charAt(l)-'a']--;
+            l++;
+            if(Arrays.equals(count1,count2)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
